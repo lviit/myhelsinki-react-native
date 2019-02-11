e9b6bbf8ab4f29c3795b23a9bea23ffb403dcc3d
lviit <lauriviitala3000@gmail.com>
2019-02-11 21:27:07 +0200

    places and activities

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -1,16 +1,78 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { format } from "date-fns";
+
+import {
+  Container,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+  Spinner
+} from "native-base";
+import { iOSUIKit } from "react-native-typography";
 
 export default class ActivitiesScreen extends React.Component {
+  state = {
+    isLoading: false,
+    results: []
+  };
+
   static navigationOptions = {
     title: "Activities"
   };
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.setState({ isLoading: true });
+    fetch(`http://open-api.myhelsinki.fi/v1/activities/?limit=30`)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ results: json.data, isLoading: false });
+      });
+  }
+
   render() {
+    const { isLoading, results } = this.state;
+
     return (
-      <ScrollView>
-        <Text>Activities</Text>
-      </ScrollView>
+      <Container>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <List>
+            {results.map(
+              ({ description: { images, body }, name: { fi: nameFi }, id }) => (
+                <ListItem thumbnail key={id}>
+                  <Left />
+                  <Body>
+                    <Text style={iOSUIKit.title3Emphasized}>{nameFi}</Text>
+                    <Text note numberOfLines={1}>
+                      {body}
+                    </Text>
+                  </Body>
+                  <Right>
+                    <Button
+                      transparent
+                      onPress={() =>
+                        this.props.navigation.navigate("EventScreen", { id })
+                      }
+                    >
+                      <Text>View</Text>
+                    </Button>
+                  </Right>
+                </ListItem>
+              )
+            )}
+          </List>
+        )}
+      </Container>
     );
   }
 }
--- screens/PlacesScreen.js
+++ screens/PlacesScreen.js
@@ -1,16 +1,80 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { format } from "date-fns";
+
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+  Spinner
+} from "native-base";
+import { iOSUIKit } from "react-native-typography";
 
 export default class PlacesScreen extends React.Component {
+  state = {
+    isLoading: false,
+    results: []
+  };
+
   static navigationOptions = {
     title: "Places"
   };
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.setState({ isLoading: true });
+    fetch(`http://open-api.myhelsinki.fi/v1/places/?limit=30`)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ results: json.data, isLoading: false });
+      });
+  }
+
   render() {
+    const { isLoading, results } = this.state;
+
     return (
-      <ScrollView>
-        <Text>Places</Text>
-      </ScrollView>
+      <Container>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <List>
+            {results.map(
+              ({ description: { images, body }, name: { fi: nameFi }, id }) => (
+                <ListItem thumbnail key={id}>
+                  <Left />
+                  <Body>
+                    <Text style={iOSUIKit.title3Emphasized}>{nameFi}</Text>
+                    <Text note numberOfLines={1}>
+                      {body}
+                    </Text>
+                  </Body>
+                  <Right>
+                    <Button
+                      transparent
+                      onPress={() =>
+                        this.props.navigation.navigate("EventScreen", { id })
+                      }
+                    >
+                      <Text>View</Text>
+                    </Button>
+                  </Right>
+                </ListItem>
+              )
+            )}
+          </List>
+        )}
+      </Container>
     );
   }
 }
