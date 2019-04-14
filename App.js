8419d2d98cca1f507da8784ece354a0be0fe16aa
lviit <lauriviitala3000@gmail.com>
2019-04-14 16:29:45 +0300

    cleanup fonts

--- App.js
+++ App.js
@@ -38,11 +38,12 @@ export default class App extends React.Component {
         // This is the font that we are using for our tab bar
         //...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        "open-sans-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
-        "open-sans-semibold": require("./assets/fonts/OpenSans-Semibold.ttf"),
-        "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-        "open-sans-extrabold": require("./assets/fonts/OpenSans-ExtraBold.ttf")
+        // to remove this if you are not using it in your app,
+        "ibm-plex-sans-condensed-bold": require("./assets/fonts/IBMPlexSansCondensed-Bold.ttf"),
+        "ibm-plex-sans-condensed-semibold": require("./assets/fonts/IBMPlexSansCondensed-SemiBold.ttf"),
+        "ibm-plex-sans-condensed-medium": require("./assets/fonts/IBMPlexSansCondensed-Medium.ttf"),
+        "ibm-plex-sans-semibold": require("./assets/fonts/IBMPlexSans-SemiBold.ttf"),
+        "ibm-plex-sans-regular": require("./assets/fonts/IBMPlexSans-Regular.ttf")
       })
     ]);
   };
